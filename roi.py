8b848c9a5f8ea9f557e9653ddd64097c3f2d7314
TSOT5 <147224452+TSOT5@users.noreply.github.com>
2023-11-21 23:07:40 -0600

    Updated roi.py

--- roi.py
+++ roi.py
@@ -1,21 +1,23 @@
-class Roi():
+class ROI:
 
     def __init__(self, price, income, costs):
         self.price = price
         self.income = income
         self.costs = costs
-    
-        yearlyincome = income * 12
-        yearlycosts = costs * 12
+
+    def calculate_roi(self):
+        yearlyincome = self.income * 12
+        yearlycosts = self.costs * 12
         yearlyincomeafter = yearlyincome - yearlycosts
-        roi = yearlyincomeafter / price
-        roiper = roi * 100
-        print(f'ROI percentage is {roiper}% per year')
-        return None
+        roi = yearlyincomeafter / self.price
+        ROIP = roi * 100
+        print(f'ROI percentage is {ROIP}% per year')
 
 
 price = float(input("What is the investment cost(Downpayment +Closing costs +Rehab budget +Misc other): "))
 income = float(input("What is the monthly income(Rental Income + Laundry Income + Storage Income + Misc Income): "))
 costs = float(input("What is the costs per month(Taxes + Insurance + Water/Sewer + Garbage + Electric/Gas + HOA Fee + Lawn/Snow + Vacancy + Repairs + CapEx + Prop. Mgmt + Mortgage): "))
 
-croi = Roi(price, income, costs)
+
+croi = ROI(price, income, costs)
+croi.calculate_roi()
